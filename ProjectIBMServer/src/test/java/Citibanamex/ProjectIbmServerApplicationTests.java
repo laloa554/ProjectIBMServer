811332d65621a34d51ee273112b730065e83c015
laloa554 <55073002+laloa554@users.noreply.github.com>
2021-10-17 14:28:19 -0500

    Add files via upload

---
+++ ProjectIBMServer/src/test/java/Citibanamex/ProjectIbmServerApplicationTests.java
@@ -0,0 +1,13 @@
+package Citibanamex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectIbmServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
